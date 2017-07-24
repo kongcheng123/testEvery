bf721cbdd6c453208019bdbf91d9b9f0b9481899
kongcheng123 <1175642794@qq.com>
2017-07-25 00:12:17 +0800

    冒泡的添加

---
+++ src/main/java/xy/dataStructure/bub/testBub.java
@@ -0,0 +1,28 @@
+package xy.dataStructure.bub;
+
+import org.junit.Test;
+
+/**
+ * Created by xy on 2017-07-24 23:50
+ * 冒泡排序
+ */
+public class testBub {
+    private int[] num={1,6,2,3,10,9,7,5,8,4};
+
+    @Test
+    public void test(){
+        for (int i = 0;i < num.length;i++){
+            for (int j = 0;j < num.length - i-1;j++){
+                if(num[j] > num[j+1]){
+                    int temp = num[j];
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
+                }
+            }
+        }
+        for (int a : num){
+            System.out.println(a);
+        }
+
+    }
+}
